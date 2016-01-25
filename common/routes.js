abb87ce3c5a61e0df3910553537ced9277254082
Kalyn Beach <kalynbeach@gmail.com>
2016-01-24 21:54:06 -0800

    Add createLoginUrl App container method

--- common/routes.js
+++ common/routes.js
@@ -3,11 +3,11 @@
  */
 
 import React from 'react'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './containers/App'
 
 export default (
-	<Router history={hashHistory}>
+	<Router history={browserHistory}>
   	<Route path="/" component={App} />
   </Router>
 )
