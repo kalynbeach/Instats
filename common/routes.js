84801b77e82029a108a80e3b086d3c8e202bb68f
Kalyn Beach <kalynbeach@gmail.com>
2016-01-24 23:24:00 -0800

    Add Instats component and lots of other stuff

--- common/routes.js
+++ common/routes.js
@@ -3,11 +3,15 @@
  */
 
 import React from 'react'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+
 import App from './containers/App'
+import Instats from './components/instats'
 
 export default (
 	<Router history={browserHistory}>
-  	<Route path="/" component={App} />
+  	<Route path="/" component={App} >
+  		<IndexRoute component={Instats} />
+  	</Route>
   </Router>
 )
